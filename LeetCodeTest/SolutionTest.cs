806489215990502877e3988334d94a2bd7cd394a
WalaaAtia <walaaatef@hotmail.com>
2022-05-15 21:13:47 +0200

    TwoSumII first attempt

--- LeetCodeTest/SolutionTest.cs
+++ LeetCodeTest/SolutionTest.cs
@@ -7,7 +7,7 @@ namespace LeetCodeTest
     public class SolutionTest
     {
         [TestMethod]
-        public void TwoSum()
+        public void TwoSumTest()
         {
             var solution = new Solution();
 
@@ -24,6 +24,28 @@ namespace LeetCodeTest
             var result3 = (solution.TwoSum(new int[] { -6, -6, 15, 15 }, 9));
 
             Assert.AreEqual(result3[0], 0);
+            Assert.AreEqual(result3[1], 2);
+
+        }
+
+        [TestMethod]
+        public void TwoSumIITest()
+        {
+            var solution = new Solution();
+
+            var result = (solution.TwoSumII(new int[] { 2, 7, 11, 15 }, 9));
+
+            Assert.AreEqual(result[0], 1);
+            Assert.AreEqual(result[1], 2);
+
+            var result2 = (solution.TwoSumII(new int[] { 2,3,4 }, 6));
+
+            Assert.AreEqual(result2[0], 1);
+            Assert.AreEqual(result2[1], 3);
+
+            var result3 = (solution.TwoSumII(new int[] { -1, 0 }, -1));
+
+            Assert.AreEqual(result3[0], 1);
             Assert.AreEqual(result3[1], 2);
 
         }
