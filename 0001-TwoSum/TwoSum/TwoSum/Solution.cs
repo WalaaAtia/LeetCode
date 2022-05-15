dfdad6e9fa7f120ed001da767829d099f49f5585
WalaaAtia <walaaatef@hotmail.com>
2022-05-15 20:22:13 +0200

    Solved

--- 0001-TwoSum/TwoSum/TwoSum/Solution.cs
+++ 0001-TwoSum/TwoSum/TwoSum/Solution.cs
@@ -1,4 +1,33 @@
-﻿using System;
+﻿//Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+//You may assume that each input would have exactly one solution, and you may not use the same element twice.
+//You can return the answer in any order.
+
+//Example 1:
+
+//Input: nums = [2, 7, 11, 15], target = 9
+//Output:[0,1]
+//Explanation: Because nums[0] +nums[1] == 9, we return [0, 1].
+//Example 2:
+
+//Input: nums = [3, 2, 4], target = 6
+//Output:[1,2]
+//Example 3:
+
+//Input: nums = [3, 3], target = 6
+//Output:[0,1]
+
+//Constraints:
+
+//2 <= nums.length <= 10^4
+//- 10^9 <= nums[i] <= 10^9
+//- 10^9 <= target <= 10^9
+//Only one valid answer exists.
+
+//Follow-up: Can you come up with an algorithm that is less than O(n2) time complexity?
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace TwoSum
 {
@@ -6,7 +35,23 @@ namespace TwoSum
     {
         public int[] TwoSum(int[] nums, int target)
         {
-            return new[] { 1, 0 };
+            var inputs = new Dictionary<int, int>();
+
+            for (int i=0; i< nums.Length; i++)
+            {
+                var complimentaryNumber = target - nums[i];
+
+                if (inputs.ContainsKey(complimentaryNumber))
+                {
+                    return new int[] { inputs[complimentaryNumber], i };
+                }
+                else 
+                {
+                    inputs.Add(nums[i], i);
+                }                
+            }
+
+            return new[] { -1, -1 };
         }
     }
 }
--- 0001-TwoSum/TwoSum/TwoSumTest/UnitTest1.cs
+++ 0001-TwoSum/TwoSum/TwoSumTest/UnitTest1.cs
@@ -16,6 +16,12 @@ namespace TwoSumTest
             Assert.AreEqual(result[0], 0);
             Assert.AreEqual(result[1], 1);
 
+
+            var result2 = (solution.TwoSum(new int[] { -6, 7, 11, 15 }, 9));
+
+            Assert.AreEqual(result2[0], 0);
+            Assert.AreEqual(result2[1], 3);
+
         }
     }
 }
